e4efaa34ac0d7f18a6e7cc4662196251f54e7931
Alvin233477 <adityawardhana62@gmail.com>
2025-09-16 10:27:08 +0700

    dana

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <?php include 'component/header.php'; ?>
 </head>
 <body>
+    <h1>test</h1>
     <?php include 'component/navbar.php'; ?>
     <?php include 'component/carausel.php'; ?>
     <?php include 'page/home.php'; ?>
